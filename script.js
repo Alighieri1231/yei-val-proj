d9124220f9df4d27f15c97dd181d4e475a58809a
Emilio Ochoa Alva <eochoaal@ur.rochester.edu>
2026-02-12 22:55:50 -0500

    .

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function handleYesClick() {
     const downloadBtn = document.createElement("a");
     downloadBtn.href = "candle.pdf"; // debe estar en la raíz del proyecto
     downloadBtn.download = "candle.pdf";
-    downloadBtn.textContent = "Download your Candle PDF 🕯️";
+    downloadBtn.textContent = "Descargalou 🕯️";
     downloadBtn.classList.add("btn", "download-pdf");
     downloadBtn.style.backgroundColor = "#845ef7";
     downloadBtn.style.marginTop = "2rem";
